6699b2646dfc31db28ca89c78d9e5531f3483b02
Andis Spriņķis <mail@andis.work>
2021-11-13 18:17:05 +0200

    winopts

--- lua/plugin_post/plugin/fzf_lua.lua
+++ lua/plugin_post/plugin/fzf_lua.lua
@@ -1,6 +1,10 @@
 local previewer = 'cat'
-
 if vim.fn.executable('bat') == 1 then previewer = 'bat' end
+
+local winopts = {
+  height = 0.955,
+  width = 0.935
+}
 
 function IsInGitDir()
   return vim.fn.system('git rev-parse --git-dir') == '.git\n'
@@ -8,20 +12,20 @@ end
 
 function ShowFiles(withGit)
   if (withGit) then
-    require('fzf-lua').git_files({ previewer = previewer })
+    require('fzf-lua').git_files({ previewer = previewer, winopts = winopts, git_icons = false })
   else
-    require('fzf-lua').files({ previewer = previewer })
+    require('fzf-lua').files({ previewer = previewer, winopts = winopts })
   end
 end
 
 function ShowGrep()
-  require('fzf-lua').live_grep({ previewer = previewer })
+  require('fzf-lua').grep({ previewer = previewer, winopts = winopts, search = '' })
 end
 
-if not vim.g.plug_requirement.git_plugins then
-  vim.api.nvim_set_keymap('n', '<tab>', ':lua ShowFiles(false)<cr>', { noremap = true, silent = true })
-else
+if vim.g.plug_requirement.git_plugins then
   vim.api.nvim_set_keymap('n', '<tab>', ':lua ShowFiles(IsInGitDir())<cr>', { noremap = true, silent = true })
+else
+  vim.api.nvim_set_keymap('n', '<tab>', ':lua ShowFiles(false)<cr>', { noremap = true, silent = true })
 end
 
 vim.api.nvim_set_keymap('n', '<s-tab>', ':lua ShowFiles(false)<cr>', { noremap = true, silent = true })
