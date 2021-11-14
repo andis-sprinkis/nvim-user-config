bd31b7aac32fe54995aa603148f55b545e768367
Andis Spriņķis <mail@andis.work>
2021-11-14 19:18:28 +0200

    theme definition

--- lua/plugin_post/plugin/lualine_nvim.lua
+++ lua/plugin_post/plugin/lualine_nvim.lua
@@ -1,3 +1,56 @@
+local colors = {
+  base03  ='#282828',
+  base02  ='#3c3836',
+  base01  ='#504945',
+  base00  ='#665c54',
+  base0   ='#7c6f64',
+  base1   ='#928374',
+  base2   ='#a89984',
+  base3   ='#ebdbb2',
+  yellow  ='#d79921',
+  orange  ='#d65d0e',
+  red     ='#fb3934',
+  magenta ='#b16286',
+  peach   ='#d3869b',
+  blue    ='#458588',
+  cyan    ='#83a598',
+  green   ='#b8bb26',
+  white   ='#ebdbb2',
+}
+
+local theme = {
+  normal = {
+    a = { bg = colors.base2, fg = colors.base03 },
+    b = { bg = colors.base01, fg = colors.base3 },
+    c = { bg = colors.base02, fg = colors.base2 },
+  },
+  insert = {
+    a = { bg = colors.cyan, fg = colors.base03 },
+    b = { bg = colors.base01, fg = colors.base3 },
+    c = { bg = colors.base01, fg = colors.base3 },
+  },
+  visual = {
+    a = { bg = colors.yellow, fg = colors.base03 },
+    b = { bg = colors.base01, fg = colors.base3 },
+    c = { bg = colors.base0, fg = colors.base03 },
+  },
+  replace = {
+    a = { bg = colors.red, fg = colors.base03 },
+    b = { bg = colors.base01, fg = colors.base3 },
+    c = { bg = colors.base03, fg = colors.base3 },
+  },
+  command = {
+    a = { bg = colors.green, fg = colors.base03 },
+    b = { bg = colors.base01, fg = colors.base3 },
+    c = { bg = colors.base0, fg = colors.base03 },
+  },
+  inactive = {
+    a = { bg = colors.base02, fg = colors.base2 },
+    b = { bg = colors.base02, fg = colors.base2 },
+    c = { bg = colors.base02, fg = colors.base2 },
+  },
+}
+
 local a = {}
 local b = {
   'branch',
@@ -12,7 +65,7 @@ local z = { 'location' }
 require('lualine').setup {
   options = {
     icons_enabled = false,
-    theme = 'gruvbox_dark',
+    theme = theme,
     component_separators = { left = '|', right = '|' },
     section_separators = {},
     disabled_filetypes = {},
