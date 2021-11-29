6058ecc01194c7647e51b8706132e9e42f050056
Andis Spriņķis <mail@andis.work>
2021-11-29 22:23:16 +0200

    ok

--- lua/plugin_post/plugin/indent_blankline_nvim.lua
+++ lua/plugin_post/plugin/indent_blankline_nvim.lua
@@ -1,3 +1,6 @@
-vim.g.indentLine_char = '▏'
-vim.g.indentLine_first_char = '▏'
-vim.g.indentLine_fileTypeExclude = { 'help', 'terminal', 'man', 'spectre_panel' }
+require('indent_blankline').setup({
+  show_current_context_start = true,
+  blankline_char = '▏',
+  filetype_exclude = { 'help', 'terminal', 'man', 'spectre_panel' },
+  show_first_indent_level = true
+})
