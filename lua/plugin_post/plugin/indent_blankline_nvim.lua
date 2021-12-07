036f225f20dc7092f2b889a3d2e84117dad59726
Andis Spriņķis <mail@andis.work>
2021-12-07 22:03:25 +0200

    cool

--- lua/plugin_post/plugin/indent_blankline_nvim.lua
+++ lua/plugin_post/plugin/indent_blankline_nvim.lua
@@ -1,5 +1,6 @@
 require('indent_blankline').setup({
   show_current_context_start = true,
+  char = '▏',
   blankline_char = '▏',
   filetype_exclude = { 'help', 'terminal', 'man', 'spectre_panel' },
   show_first_indent_level = true
