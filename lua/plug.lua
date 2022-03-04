87fc4f58776498f6bb31d454270da49af7ef67c1
Andis Spriņķis <mail@andis.work>
2022-03-04 23:29:25 +0200

    ok

--- lua/plug.lua
+++ lua/plug.lua
@@ -82,8 +82,5 @@ if vim.g.plug_reqr.nvim_spectre then vim.cmd([[Plug 'nvim-pack/nvim-spectre']]) 
 vim.fn['plug#end']()
 
 vim.cmd([[
-  autocmd VimEnter *
-    \  if len(filter(values(g:plugs), '!isdirectory(v:val.dir)'))
-    \|   PlugInstall --sync | q
-    \| endif
+  autocmd VimEnter * if len(filter(values(g:plugs), '!isdirectory(v:val.dir)')) | PlugInstall --sync | q | endif
 ]])
