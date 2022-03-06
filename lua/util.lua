a8b00ca7cc965b6484a2d77d8ff220884156357d
Andis Spriņķis <mail@andis.work>
2022-03-06 12:29:59 +0200

    ok

--- lua/util.lua
+++ lua/util.lua
@@ -1,26 +1,26 @@
-local function has_module(modules)
-  if modules == nil then return true end
+local function is_module_available(name)
+  if package.loaded[name] then return true end
 
-  local function is_module_available(name)
-    if package.loaded[name] then
+  for _, searcher in ipairs(package.searchers or package.loaders) do
+    local loader = searcher(name)
+
+    if type(loader) == 'function' then
+      package.preload[name] = loader
+
       return true
-    else
-      for _, searcher in ipairs(package.searchers or package.loaders) do
-        local loader = searcher(name)
-        if type(loader) == 'function' then
-          package.preload[name] = loader
-          return true
-        end
-      end
-      return false
     end
   end
+
+  return false
+end
+
+local function has_modules(modules)
+  if modules == nil then return true end
 
   for _, value in ipairs(modules) do if not is_module_available[value] then return false end end
 
   return true
 end
-
 
 local function has_sys_reqr(requirements)
   if requirements == nil then return true end
@@ -49,6 +49,7 @@ end
 local function with_reqr (args)
   if (args.dependency.sys_reqr ~= nil and not has_sys_reqr(args.dependency.sys_reqr))
     or (args.dependency.plugs ~= nil and not has_plugs(args.dependency.plugs))
+    or (args.dependency.modules ~= nil and not has_modules(args.dependency.modules))
   then
     call_cb(args.dependant.unmet_cb)
     return
@@ -59,7 +60,7 @@ local function with_reqr (args)
 end
 
 return {
-  has_module = has_module,
+  has_modules = has_modules,
   has_sys_reqr = has_sys_reqr,
   has_plugs = has_plugs,
   with_reqr = with_reqr
