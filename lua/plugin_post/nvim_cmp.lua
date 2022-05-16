96ac046cdab89827d376d69e8d05c92ddefd2e87
Andis Spriņķis <mail@andis.work>
2022-05-16 11:02:38 +0300

    whitespace

--- lua/plugin_post/nvim_cmp.lua
+++ lua/plugin_post/nvim_cmp.lua
@@ -54,7 +54,7 @@ return function ()
         if cmp.visible() then cmp.select_next_item()
         elseif luasnip.expand_or_jumpable() then luasnip.expand_or_jump()
         elseif has_words_before() then cmp.complete()
-        else fallback() end 
+        else fallback() end
       end, { "i", "s" }),
       ["<S-Tab>"] = cmp.mapping(function(fallback)
         if cmp.visible() then cmp.select_prev_item()
