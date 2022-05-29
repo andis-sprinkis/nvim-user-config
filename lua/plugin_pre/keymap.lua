583d73944ed9fc3d43b8865c08e715717396544d
Andis Spriņķis <mail@andis.work>
2022-05-29 21:19:56 +0300

    split keys to align with other programs (tmux, i3)

--- lua/plugin_pre/keymap.lua
+++ lua/plugin_pre/keymap.lua
@@ -19,8 +19,8 @@ vim.api.nvim_set_keymap('v', 'l', '<space>', { noremap = true })
 -- vim.api.nvim_set_keymap('n', '<C-h>', '<C-W><C-H>', { noremap = true })
 
 -- creating splits
-vim.api.nvim_set_keymap('n', '<leader>s', ':split<cr>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>vs', ':vsplit<cr>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>v', ':split<cr>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>o', ':vsplit<cr>', { noremap = true, silent = true })
 
 -- insert to normal mode in terminal
 vim.api.nvim_set_keymap('t', '<C-w>', '<C-\\><C-n>', { noremap = true })
