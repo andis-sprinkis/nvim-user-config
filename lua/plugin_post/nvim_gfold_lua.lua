a46d70ed6c86af5c06386bd81530c9b616a7c10f
Andis Spriņķis <mail@andis.work>
2022-05-29 10:30:20 +0300

    gfold browse repo on switch

--- lua/plugin_post/nvim_gfold_lua.lua
+++ lua/plugin_post/nvim_gfold_lua.lua
@@ -3,7 +3,12 @@ return function()
     picker = {
       -- what to do when selecting a repo
       -- by default changes cwd
-      on_select = require('gfold.actions').change_cwd,
+      on_select = function (repo, idx)
+        require('gfold.actions').change_cwd(repo, idx)
+        if repo then
+          vim.cmd("edit " .. repo.path)
+        end
+      end,
     },
   })
   vim.cmd([[command! Gfold execute "lua require('gfold').pick_repo()"]])
