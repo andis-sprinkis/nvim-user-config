36364537c9634b93a7db6ef24066487a3c11273f
Andis Spriņķis <mail@andis.work>
2022-05-30 22:13:09 +0300

    ok:

--- lua/plugin_post/nvim_context_vt_nvim.lua
+++ lua/plugin_post/nvim_context_vt_nvim.lua
@@ -1,5 +1,10 @@
 return function ()
   require("nvim_context_vt").setup({
-    prefix = ' In',
+    custom_parser = function(node, ft, opts)
+      local utils = require('nvim_context_vt.utils')
+      if node:type() == 'function' then return nil end
+      return '  In "' .. utils.get_node_text(node)[1] .. '"'
+    end,
+    highlight = 'Whitespace',
   })
 end
