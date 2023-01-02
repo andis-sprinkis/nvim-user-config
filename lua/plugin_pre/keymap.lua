7574b92aab0f48ffc4aff43388c2775a5a67207f
Andis Spriņķis <spr.andis@protonmail.com>
2023-01-03 00:17:26 +0200

    quit buffer w/ esc in normal mode keymap

--- lua/plugin_pre/keymap.lua
+++ lua/plugin_pre/keymap.lua
@@ -10,5 +10,8 @@ vim.api.nvim_set_keymap('v', 'l', '<space>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>v', ':split<cr>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>o', ':vsplit<cr>', { noremap = true, silent = true })
 
+-- quit buffer
+vim.api.nvim_set_keymap('n', '<esc>', ':q<cr>', { noremap = true, silent = true })
+
 -- insert to normal mode in terminal
 vim.api.nvim_set_keymap('t', '<C-w>', '<C-\\><C-n>', { noremap = true })
