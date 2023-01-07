4a5584c8ff4807787c9d05a3439815ada1097f74
Andis Spriņķis <spr.andis@protonmail.com>
2023-01-07 14:02:17 +0200

    ok

--- lua/option.lua
+++ lua/option.lua
@@ -1,7 +1,5 @@
-if vim.g.os == nil then
-  if (vim.fn.has('win64') == 1) then vim.g.os = 'Windows'
-  else vim.g.os = vim.fn.substitute(vim.fn.system('uname'), '\n', '', '') end
-end
+if (vim.fn.has('win64') == 1) then vim.g.os = 'Windows'
+else vim.g.os = vim.fn.substitute(vim.fn.system('uname'), '\n', '', '') end
 
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
--- lua/pack.lua
+++ lua/pack.lua
@@ -17,14 +17,21 @@ vim.g.sys_reqr = {
   vim_hexokinase = vim.fn.executable('go') == 1,
 }
 
-local packer_bootstrap
-local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  vim.o.runtimepath = vim.fn.stdpath('data') .. '/site/pack/*/start/*,' .. vim.o.runtimepath
-  packer_bootstrap = vim.fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim',
-    install_path })
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+    vim.cmd [[packadd packer.nvim]]
+
+    return true
+  end
+
+  return false
 end
+
+local packer_bootstrap = ensure_packer()
 
 require('packer').startup(function(use)
   use {
