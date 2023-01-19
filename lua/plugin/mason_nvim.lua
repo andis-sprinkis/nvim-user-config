796da0120013536a870d82936cb40fe9c5addf5e
Andis Spriņķis <spr.andis@protonmail.com>
2023-01-20 00:12:13 +0200

    ok

--- lua/plugin/mason_nvim.lua
+++ lua/plugin/mason_nvim.lua
@@ -64,12 +64,8 @@ return function()
   end
 
   local function make_config()
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-
-    capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
-
     return {
-      capabilities = capabilities,
+      capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities()),
       on_attach = on_attach,
     }
   end
