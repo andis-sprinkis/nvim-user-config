bb170d0a37a609963c51914d678e1a591f891dbc
Andis Spriņķis <spr.andis@protonmail.com>
2023-01-22 22:02:17 +0200

    ok

--- lua/plugin/nvim_context_vt_nvim.lua
+++ lua/plugin/nvim_context_vt_nvim.lua
@@ -3,8 +3,8 @@ return function()
   local context_vt_utils = require('nvim_context_vt.utils')
 
   context_vt.setup({
-    custom_parser = function(node, ft, opts)
-      return (node:type() ~= 'function') and ' In "' .. context_vt_utils.get_node_text(node)[1] .. '"'
+    custom_parser = function(node)
+      return ' In "' .. context_vt_utils.get_node_text(node)[1] .. '"'
     end,
     highlight = 'Whitespace',
     disable_virtual_lines = true,
