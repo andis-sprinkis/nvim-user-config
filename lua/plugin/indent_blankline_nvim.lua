dc8a32dc2ad0424625bbe9ad8170ad938318516b
Andis Spriņķis <spr.andis@protonmail.com>
2023-01-22 19:34:25 +0200

    ok

--- lua/plugin/indent_blankline_nvim.lua
+++ lua/plugin/indent_blankline_nvim.lua
@@ -3,6 +3,7 @@ return function()
     show_current_context = true,
     show_current_context_start = true,
     show_first_indent_level = true,
+    show_end_of_line = true,
     char = '▏',
     blankline_char = '▏',
     filetype_exclude = {
