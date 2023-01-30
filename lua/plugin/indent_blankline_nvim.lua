4c422cd1361f9ecece621f64d812975305203b11
Andis Spriņķis <spr.andis@protonmail.com>
2023-01-31 00:51:34 +0200

    ok

--- lua/plugin/indent_blankline_nvim.lua
+++ lua/plugin/indent_blankline_nvim.lua
@@ -2,20 +2,14 @@ return function()
   require('indent_blankline').setup({
     show_current_context = true,
     show_current_context_start = true,
+    show_current_context_start_on_current_line = false,
     show_end_of_line = true,
     show_first_indent_level = true,
     char = '▏',
     blankline_char = '▏',
     context_char = '▏',
     filetype_exclude = {
-      '',
-      'Fm',
-      'help',
-      'lsp-installer',
-      'man',
-      'packer',
       'spectre_panel',
-      'terminal',
     },
   })
 end
