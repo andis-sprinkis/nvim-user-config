d3ed8e35ecb606f1840514891de6a5dd52fe681e
Andis Spriņķis <spr.andis@protonmail.com>
2023-10-03 12:35:41 +0300

    ok

--- lua/plugin/indent_blankline_nvim.lua
+++ lua/plugin/indent_blankline_nvim.lua
@@ -1,18 +1,20 @@
 local M = {
   'lukas-reineke/indent-blankline.nvim',
+  main = 'ibl',
   config = function()
-    require('indent_blankline').setup({
-      show_current_context = true,
-      show_current_context_start = true,
-      show_current_context_start_on_current_line = false,
-      show_end_of_line = true,
-      show_first_indent_level = true,
-      char = '▏',
-      blankline_char = '▏',
-      context_char = '▏',
-      filetype_exclude = {
-        'help',
-        'spectre_panel'
+    require('ibl').setup({
+      indent = {
+        char = { '▏' },
+      },
+      scope = {
+        char = { '▏' },
+        highlight = { "Operator" },
+      },
+      exclude = {
+        filetypes = {
+          'help',
+          'spectre_panel'
+        },
       },
     })
   end,
