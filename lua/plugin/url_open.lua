9f7b510da3f27a942fb4a69fce43271256b9cbbe
Andis Spriņķis <spr.andis@protonmail.com>
2023-10-16 11:12:45 +0300

    ok

--- lua/plugin/url_open.lua
+++ lua/plugin/url_open.lua
@@ -2,7 +2,9 @@ local M = {
   'sontungexpt/url-open',
   event = 'VeryLazy',
   config = function()
-    require("url-open").setup({})
+    require("url-open").setup({
+      deep_pattern = true
+    })
     vim.keymap.set("n", "<cr>", "<esc>:URLOpenUnderCursor<cr>")
   end,
 }
