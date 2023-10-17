5917d89b7eeaa2dd7bf7aa8c74ce75973144e563
Andis Spriņķis <spr.andis@protonmail.com>
2023-10-17 09:32:53 +0300

    ok

--- lua/plugin/url_open.lua
+++ lua/plugin/url_open.lua
@@ -3,7 +3,18 @@ local M = {
   event = 'VeryLazy',
   config = function()
     require("url-open").setup({
-      deep_pattern = true
+      deep_pattern = true,
+      highlight_url = {
+        all_urls = {
+          enabled = false
+        },
+        cursor_move = {
+          enabled = true,
+          fg = 'text',
+          bg = nil,
+          underline = true
+        }
+      },
     })
     vim.keymap.set("n", "<Leader>l", "<esc>:URLOpenUnderCursor<cr>")
   end,
