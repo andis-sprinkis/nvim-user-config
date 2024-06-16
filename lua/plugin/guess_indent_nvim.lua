e08f50652332d894ce01290cc4c78166e95f5117
Andis Spriņķis <spr.andis@protonmail.com>
2024-06-16 15:19:39 +0300

    ok

--- lua/plugin/guess_indent_nvim.lua
+++ lua/plugin/guess_indent_nvim.lua
@@ -1,6 +1,14 @@
 local M = {
   'NMAC427/guess-indent.nvim',
-  config = true,
+  config = function()
+    require('guess-indent').setup {
+      filetype_exclude = {
+        "dirvish",
+        "fugitive",
+        "lazy",
+      }
+    }
+  end,
   lazy = false,
   priority = 900
 }
