62d2e698c77c3d249b82596b75027c30def03595
Andis Spriņķis <spr.andis@protonmail.com>
2024-07-04 18:56:43 +0300

    ok

--- lua/plugin/matchparen_nvim.lua
+++ lua/plugin/matchparen_nvim.lua
@@ -1,6 +1,26 @@
 local M = {
   'monkoose/matchparen.nvim',
-  config = true, -- TODO: autocommand to detach from large buffers
+  config = function ()
+    require('matchparen').setup({
+      on_startup = false
+    })
+
+    local ag = vim.api.nvim_create_augroup
+    local ac = vim.api.nvim_create_autocmd
+
+    local ag_matchparen_nvim_cfg = ag('matchparen_nvim_cfg', {})
+
+    ac({ 'BufRead' }, {
+      group = ag_matchparen_nvim_cfg,
+      callback = function()
+        if vim.b.large_file_buf then
+          vim.cmd("MatchParenDisable")
+        else
+          vim.cmd("MatchParenEnable")
+        end
+      end
+    })
+  end,
   event = 'VeryLazy'
 }
 
