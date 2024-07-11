43a98de8164355126012de8c5e774a9b6407122f
Andis Spriņķis <spr.andis@protonmail.com>
2024-07-11 17:00:17 +0300

    ok

--- lua/plugin/fzf_lua.lua
+++ lua/plugin/fzf_lua.lua
@@ -12,21 +12,7 @@ local M = {
     local fzflua = require('fzf-lua')
 
     fzflua.setup({
-      fzf_colors = {
-        ['fg'] = { 'fg', 'CursorLine' },
-        ['bg'] = { 'bg', 'Normal' },
-        ['hl'] = { 'fg', 'Comment' },
-        ['fg+'] = { 'fg', 'Normal' },
-        ['bg+'] = { 'bg', 'CursorLine' },
-        ['hl+'] = { 'fg', 'Statement' },
-        ['info'] = { 'fg', 'PreProc' },
-        ['prompt'] = { 'fg', 'Conditional' },
-        ['pointer'] = { 'fg', 'Exception' },
-        ['marker'] = { 'fg', 'Keyword' },
-        ['spinner'] = { 'fg', 'Label' },
-        ['header'] = { 'fg', 'Comment' },
-        ['gutter'] = { 'bg', 'Normal' },
-      },
+      fzf_colors = true,
       previewers = {
         cat = {
           cmd = 'cat',
