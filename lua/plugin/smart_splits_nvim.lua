5ae105466ab5cf8a9e7a3b08037c78815d86924e
Andis Spriņķis <spr.andis@protonmail.com>
2024-07-29 22:30:15 +0300

    ok

--- lua/plugin/smart_splits_nvim.lua
+++ lua/plugin/smart_splits_nvim.lua
@@ -9,15 +9,16 @@ local M = {
     })
 
     -- resizing splits
-    km('n', '<C-A-h>', smart_splits.resize_left, { desc = 'Dunno (smart-splits)' })
-    km('n', '<C-A-j>', smart_splits.resize_down, { desc = 'Dunno (smart-splits)' })
-    km('n', '<C-A-k>', smart_splits.resize_up, { desc = 'Dunno (smart-splits)' })
-    km('n', '<C-A-l>', smart_splits.resize_right, { desc = 'Dunno (smart-splits)' })
+    km('n', '<C-A-h>', smart_splits.resize_left, { desc = 'Change window size to the left direction (smart-splits)' })
+    km('n', '<C-A-j>', smart_splits.resize_down, { desc = 'Change window size to the down direction (smart-splits)' })
+    km('n', '<C-A-k>', smart_splits.resize_up, { desc = 'Change window size to the up direction (smart-splits)' })
+    km('n', '<C-A-l>', smart_splits.resize_right, { desc = 'Change window size to the right direction (smart-splits)' })
     -- moving between splits
-    km('n', '<C-h>', smart_splits.move_cursor_left, { desc = 'Dunno (smart-splits)' })
-    km('n', '<C-j>', smart_splits.move_cursor_down, { desc = 'Dunno (smart-splits)' })
-    km('n', '<C-k>', smart_splits.move_cursor_up, { desc = 'Dunno (smart-splits)' })
-    km('n', '<C-l>', smart_splits.move_cursor_right, { desc = 'Dunno (smart-splits)' })
+    km('n', '<C-h>', smart_splits.move_cursor_left, { desc = 'Move cursor to window left of current one (smart-splits)' })
+    km('n', '<C-j>', smart_splits.move_cursor_down, { desc = 'Move cursor to window below current one (smart-splits)' })
+    km('n', '<C-k>', smart_splits.move_cursor_up, { desc = 'Move cursor to window above current one (smart-splits)' })
+    km('n', '<C-l>', smart_splits.move_cursor_right,
+      { desc = 'Move cursor to window right of current one (smart-splits)' })
   end,
   keys = {
     { '<C-A-h>', mode = 'n' },
