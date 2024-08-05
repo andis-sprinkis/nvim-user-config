c2b0c282d9fca8d1cd3839f0d6ea6fb21fc2f4ba
Andis Spriņķis <spr.andis@protonmail.com>
2024-08-05 17:24:45 +0300

    ok

--- lua/plugin/markdown_preview.lua
+++ lua/plugin/markdown_preview.lua
@@ -1,12 +1,12 @@
 local g = vim.g
-local fn = vim.fn
 local sys_reqr = g.sys_reqr
 
 local M = {
   'iamcco/markdown-preview.nvim',
   cond = sys_reqr.markdown_preview,
   enabled = sys_reqr.markdown_preview,
-  build = function() fn['mkdp#util#install']() end,
+  build = function() vim.cmd[[call mkdp#util#install()]] end,
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
   ft = { 'markdown', 'markdown.mdx' },
 }
 
--- lua/plugin/vim_doge.lua
+++ lua/plugin/vim_doge.lua
@@ -1,12 +1,11 @@
 local g = vim.g
-local fn = vim.fn
 local sys_reqr = g.sys_reqr
 
 local M = {
   'kkoomen/vim-doge',
   cond = sys_reqr.vim_doge,
   enabled = sys_reqr.vim_doge,
-  build = function() fn['doge#install']() end,
+  build = function() vim.cmd[[call doge#install()]] end,
   init = function()
     g.doge_enable_mappings = 0
   end,
