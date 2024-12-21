f2717e68696d1ee07ac67f590086d50e75c90e61
Andis Spriņķis <andis@sprinkis.com>
2024-12-21 19:11:45 +0200

    ok

--- lua/plugin/mason_nvim.lua
+++ lua/plugin/mason_nvim.lua
@@ -30,6 +30,14 @@ local M = {
       'yamlls',
     }
 
+    if sys_reqr.lsp_neocmake then
+      table.insert(lsp_servers, 'neocmake')
+    end
+
+    if sys_reqr.lsp_asm then
+      table.insert(lsp_servers, 'asm_lsp')
+    end
+
     local dap_providers = {
       -- 'codelldb',
       -- 'python',
@@ -41,14 +49,6 @@ local M = {
       'shellcheck',
       'checkmake'
     }
-
-    if sys_reqr.lsp_neocmake then
-      table.insert(lsp_servers, 'neocmake')
-    end
-
-    if sys_reqr.lsp_asm then
-      table.insert(lsp_servers, 'asm_lsp')
-    end
 
     -- Mappings.
     -- See `:help vim.diagnostic.*` for documentation on any of the below functions
