7c7c7fc8b431e040575f12b6183342e68894a531
Andis Spriņķis <andis@sprinkis.com>
2025-03-27 17:34:36 +0200

    ok

---
+++ lua/plugin/codecompanion_nvim.lua
@@ -0,0 +1,24 @@
+local M = {
+  "olimorris/codecompanion.nvim",
+  config = function()
+    vim.env["CODECOMPANION_TOKEN_PATH"] = vim.fn.expand("~/.config")
+  end,
+  opts = {
+    -- https://github.com/olimorris/codecompanion.nvim/blob/main/lua/codecompanion/config.lua
+    strategies = {
+      chat = { adapter = "copilot" },
+      inline = { adapter = "copilot" },
+    },
+    opts = {
+      log_level = "DEBUG",
+    },
+  },
+  dependencies = {
+    "hrsh7th/nvim-cmp",
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    -- "github/copilot.vim"
+  }
+}
+
+return M
