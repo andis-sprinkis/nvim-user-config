2c87f7716d0019c5bf0f54739e0b311ea886a952
Andis Spriņķis <andis@sprinkis.com>
2025-03-29 21:57:51 +0200

    ok

--- lua/plugin/colorscheme.lua
+++ lua/plugin/colorscheme.lua
@@ -2,8 +2,15 @@ local M = {
   'Mofiqul/vscode.nvim',
   config = function()
     local colorscheme_vscode = require('vscode')
+    local c = require('vscode.colors').get_colors()
+    local isDark = vim.o.background == 'dark'
+
     colorscheme_vscode.setup({
-      italic_comments = true
+      italic_comments = true,
+      group_overrides = {
+        NormalFloat = { bg = c.vscTabOutside },
+        Pmenu = { fg = isDark and c.vscTabOutside or 'none', bg = c.vscPopupBack }
+      }
     })
     colorscheme_vscode.load()
   end,
