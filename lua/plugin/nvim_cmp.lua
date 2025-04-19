06c7c6ddaf74242a0801495d66ea72761ea2ec41
Andis Spriņķis <andis@sprinkis.com>
2025-04-19 14:00:52 +0300

    ok

--- lua/plugin/nvim_cmp.lua
+++ lua/plugin/nvim_cmp.lua
@@ -58,33 +58,13 @@ local M = {
           require('luasnip').lsp_expand(args.body)
         end,
       },
-      mapping = {
-        ['<C-b>'] = cmpm.scroll_docs(-4),
-        ['<C-f>'] = cmpm.scroll_docs(4),
-        ['<C-Space>'] = cmpm.complete(),
-        ['<C-e>'] = cmpm.abort(),
-        ['<CR>'] = cmpm.confirm({ select = false }),
-        ['<Tab>'] = cmpm(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          elseif luasnip.expand_or_jumpable() then
-            luasnip.expand_or_jump()
-          elseif has_words_before() then
-            cmp.complete()
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-        ['<S-Tab>'] = cmpm(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-      },
+      mapping = cmp.mapping.preset.insert({
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      }),
       sources = cmpc.sources(
         {
           { name = 'copilot' },
