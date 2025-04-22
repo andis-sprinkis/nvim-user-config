4ba7afff3cbf9f1b3d2ce950c27b68b6a2d45197
Andis Spriņķis <andis@sprinkis.com>
2025-04-22 18:09:18 +0300

    ok

--- lua/plugin/nvim_lightbulb.lua
+++ lua/plugin/nvim_lightbulb.lua
@@ -1,12 +1,18 @@
 local M = {
   'kosayoda/nvim-lightbulb',
-  enabled = false,
   config = function()
     require("nvim-lightbulb").setup({
       autocmd = { enabled = true },
       ignore = {
         clients = { "null-ls" },
-      }
+      },
+      ---@type (fun(client_name:string, result:lsp.CodeAction|lsp.Command):boolean)|nil
+      filter = function (client, result)
+        if (client == 'ts_ls') then
+          --
+        end
+        return true
+      end
     })
   end
 }
