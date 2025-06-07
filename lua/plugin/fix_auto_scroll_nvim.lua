9efd513538056d82e065ff02c112831b4555061c
Andis Spriņķis <andis@sprinkis.com>
2025-06-07 19:09:02 +0300

    ok

--- lua/plugin/fix_auto_scroll_nvim.lua
+++ lua/plugin/fix_auto_scroll_nvim.lua
@@ -1,3 +1,5 @@
+-- [window view topline is not preserved when switching buffers · Issue #26828 · neovim/neovim](https://github.com/neovim/neovim/issues/26828)
+
 local M = {
   'BranimirE/fix-auto-scroll.nvim',
   config = true,
