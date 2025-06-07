df4247f0a2a919d32af67cb864b77a5b3660374d
Andis Spriņķis <andis@sprinkis.com>
2025-06-08 01:54:36 +0300

    ok

--- lua/plugin/mason_nvim.lua
+++ lua/plugin/mason_nvim.lua
@@ -138,8 +138,6 @@ local M = {
     local fn_default_capabilities = require("cmp_nvim_lsp").default_capabilities
     -- local fn_default_capabilities = require('blink.cmp').get_lsp_capabilities
 
-    -- require("mason-lock").setup()
-
     lsp.config('*', {
       capabilities = fn_default_capabilities(),
       on_attach = on_attach,
