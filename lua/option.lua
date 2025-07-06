5f57814528931228f88c125ae9f58b430a5af525
Andis Spriņķis <andis@sprinkis.com>
2025-07-06 09:43:46 +0300

    ok

--- lua/option.lua
+++ lua/option.lua
@@ -166,6 +166,7 @@ ac(
     group = ag_option,
     pattern = { 'netrw' },
     callback = function()
+      km('n', '-', ':let @/=expand("%:t") <Bar> execute \'Explore\' expand("%:h") <Bar> normal n<CR>', { buffer = true })
       km('n', '=', '<Plug>NetrwLocalBrowseCheck', { buffer = true })
     end
   }
