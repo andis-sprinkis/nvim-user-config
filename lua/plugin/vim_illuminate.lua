255290885b22c7486b3b7a4ff49717e9ea111ad6
Andis Spriņķis <andis@sprinkis.com>
2025-07-31 01:18:48 +0300

    ok

--- lua/plugin/vim_illuminate.lua
+++ lua/plugin/vim_illuminate.lua
@@ -4,6 +4,7 @@ local M = {
   config = function()
     require('illuminate').configure({
       filetypes_denylist = {},
+      delay = 500,
       should_enable = function(buf)
         return not vim.b.large_file_buf
       end
