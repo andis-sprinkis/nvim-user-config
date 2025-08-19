264b1083dc42fde6b172fca3e715f01eaa2c4856
Andis Spriņķis <andis@sprinkis.com>
2025-08-19 20:57:26 +0300

    ok

--- lua/plugin/swenv_nvim.lua
+++ lua/plugin/swenv_nvim.lua
@@ -2,13 +2,7 @@ local M = {
   'AckslD/swenv.nvim',
   config = function()
     require('swenv').setup({
-      -- Path passed to `get_venvs`.
       venvs_path = vim.fn.expand('~/.local/share/virtualenvs'),
-      -- Should return a list of tables with a `name` and a `path` entry each.
-      -- Gets the argument `venvs_path` set below.
-      -- By default just lists the entries in `venvs_path`.
-      get_venvs = function(venvs_path) return require('swenv.api').get_venvs(venvs_path) end,
-      -- Something to do after setting an environment
       post_set_venv = function() vim.cmd.LspRestart() end
     })
 
