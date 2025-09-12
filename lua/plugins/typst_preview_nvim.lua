44b7345e8fb070301001839c3d12f17c77f20877
Andis Spriņķis <andis@sprinkis.com>
2025-09-12 23:58:34 +0300

    ok

--- lua/plugins/typst_preview_nvim.lua
+++ lua/plugins/typst_preview_nvim.lua
@@ -1,10 +1,8 @@
-local sys_reqr = vim.g.sys_reqr
-
 local M = {
   'chomosuke/typst-preview.nvim',
   ft = 'typst',
   version = '1.*',
-  enabled = sys_reqr.typst_preview_nvim,
+  enabled = vim.g.sys_reqr.typst_preview_nvim,
   build = function() require 'typst-preview'.update() end,
   config = function()
     require 'typst-preview'.setup {
