30ccc4e74ac8b95a3b87eed1efd15b1b4d85af4f
Andis Spriņķis <andis@sprinkis.com>
2025-09-24 23:17:54 +0300

    ok

--- lua/plugins/smart_splits_nvim.lua
+++ lua/plugins/smart_splits_nvim.lua
@@ -12,21 +12,23 @@ local M = {
     km('n', '<C-A-j>', smart_splits.resize_down, { desc = 'Change window size to the down direction (smart-splits)' })
     km('n', '<C-A-k>', smart_splits.resize_up, { desc = 'Change window size to the up direction (smart-splits)' })
     km('n', '<C-A-l>', smart_splits.resize_right, { desc = 'Change window size to the right direction (smart-splits)' })
-    km('n', '<leader>h', smart_splits.move_cursor_left, { desc = 'Move cursor to window left of current one (smart-splits)' })
-    km('n', '<leader>j', smart_splits.move_cursor_down, { desc = 'Move cursor to window below current one (smart-splits)' })
+    km('n', '<leader>h', smart_splits.move_cursor_left,
+      { desc = 'Move cursor to window left of current one (smart-splits)' })
+    km('n', '<leader>j', smart_splits.move_cursor_down,
+      { desc = 'Move cursor to window below current one (smart-splits)' })
     km('n', '<leader>k', smart_splits.move_cursor_up, { desc = 'Move cursor to window above current one (smart-splits)' })
     km('n', '<leader>l', smart_splits.move_cursor_right,
       { desc = 'Move cursor to window right of current one (smart-splits)' })
   end,
   keys = {
-    { '<C-A-h>', mode = 'n' },
-    { '<C-A-j>', mode = 'n' },
-    { '<C-A-k>', mode = 'n' },
-    { '<C-A-l>', mode = 'n' },
-    { '<leader>h',   mode = 'n' },
-    { '<leader>j',   mode = 'n' },
-    { '<leader>k',   mode = 'n' },
-    { '<leader>l',   mode = 'n' },
+    { '<C-A-h>',   mode = 'n' },
+    { '<C-A-j>',   mode = 'n' },
+    { '<C-A-k>',   mode = 'n' },
+    { '<C-A-l>',   mode = 'n' },
+    { '<leader>h', mode = 'n' },
+    { '<leader>j', mode = 'n' },
+    { '<leader>k', mode = 'n' },
+    { '<leader>l', mode = 'n' },
   }
 }
 
