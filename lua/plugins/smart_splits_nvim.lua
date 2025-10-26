34391ad5c4802ab0816abb6d820dc4758536cc13
Andis Spriņķis <andis@sprinkis.com>
2025-10-26 10:44:49 +0200

    ok

--- lua/plugins/smart_splits_nvim.lua
+++ lua/plugins/smart_splits_nvim.lua
@@ -1,6 +1,6 @@
 local M = {
   'https://github.com/mrjones2014/smart-splits.nvim',
-  init = function ()
+  init = function()
     vim.g.smart_splits_multiplexer_integration = false
   end,
   config = function()
@@ -12,6 +12,7 @@ local M = {
     km('n', '<C-A-j>', smart_splits.resize_down, { desc = 'Change window size to the down direction (smart-splits)' })
     km('n', '<C-A-k>', smart_splits.resize_up, { desc = 'Change window size to the up direction (smart-splits)' })
     km('n', '<C-A-l>', smart_splits.resize_right, { desc = 'Change window size to the right direction (smart-splits)' })
+
     km('n', '<leader>h', smart_splits.move_cursor_left,
       { desc = 'Move cursor to window left of current one (smart-splits)' })
     km('n', '<leader>j', smart_splits.move_cursor_down,
@@ -19,16 +20,30 @@ local M = {
     km('n', '<leader>k', smart_splits.move_cursor_up, { desc = 'Move cursor to window above current one (smart-splits)' })
     km('n', '<leader>l', smart_splits.move_cursor_right,
       { desc = 'Move cursor to window right of current one (smart-splits)' })
+
+    local swap_opts = { move_cursor = true }
+    km('n', '<leader><leader>h', function() smart_splits.swap_buf_left(swap_opts) end,
+      { desc = 'Swap window with the one in the left direction (smart-splits)' })
+    km('n', '<leader><leader>j', function() smart_splits.swap_buf_down(swap_opts) end,
+      { desc = 'Swap window with the one in the down direction (smart-splits)' })
+    km('n', '<leader><leader>k', function() smart_splits.swap_buf_up(swap_opts) end,
+      { desc = 'Swap window with the one in the up direction (smart-splits)' })
+    km('n', '<leader><leader>l', function() smart_splits.swap_buf_right(swap_opts) end,
+      { desc = 'Swap window with the one in the right direction (smart-splits)' })
   end,
   keys = {
-    { '<C-A-h>',   mode = 'n' },
-    { '<C-A-j>',   mode = 'n' },
-    { '<C-A-k>',   mode = 'n' },
-    { '<C-A-l>',   mode = 'n' },
-    { '<leader>h', mode = 'n' },
-    { '<leader>j', mode = 'n' },
-    { '<leader>k', mode = 'n' },
-    { '<leader>l', mode = 'n' },
+    { '<C-A-h>',           mode = 'n' },
+    { '<C-A-j>',           mode = 'n' },
+    { '<C-A-k>',           mode = 'n' },
+    { '<C-A-l>',           mode = 'n' },
+    { '<leader>h',         mode = 'n' },
+    { '<leader>j',         mode = 'n' },
+    { '<leader>k',         mode = 'n' },
+    { '<leader>l',         mode = 'n' },
+    { '<leader><leader>h', mode = 'n' },
+    { '<leader><leader>j', mode = 'n' },
+    { '<leader><leader>k', mode = 'n' },
+    { '<leader><leader>l', mode = 'n' },
   }
 }
 
