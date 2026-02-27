72722bb5b5626381f55bf96980949de733866e7d
Andis Spriņķis <andis@sprinkis.com>
2026-02-27 19:29:03 +0200

    ok

--- lua/plugins/_virt_fast_cursor_move.lua
+++ lua/plugins/_virt_fast_cursor_move.lua
@@ -6,70 +6,47 @@ local M = {
     -- Adapted from https://github.com/xiyaowong/fast-cursor-move.nvim (license: MIT).
 
     local fn = vim.fn
+    local km = vim.keymap.set
 
     local accel_v = { 7, 14, 20, 26, 31, 36, 40 }
     local accel_h = { 10, 15, 20 }
 
-    local accel = {
-      h = accel_h,
-      j = accel_v,
-      k = accel_v,
-      l = accel_h,
-      ['<Left>'] = accel_h,
-      ['<Down>'] = accel_v,
-      ['<Up>'] = accel_v,
-      ['<Right>'] = accel_h,
-    }
-
-    local chars = {
-      h = "h",
-      j = "gj",
-      k = "gk",
-      l = "l",
-      ['<Left>'] = '<Left>',
-      ['<Down>'] = 'g<Down>',
-      ['<Up>'] = 'g<Up>',
-      ['<Right>'] = '<Right>'
-    }
-
-    local prev_dir
+    local prev_key
     local prev_time = 0
     local move_count = 0
 
-    local function move(dir)
+    local function mv(key, key_remap, key_accel)
       if fn.reg_recording() ~= "" or fn.reg_executing() ~= "" or vim.v.count > 0 then
-        return chars[dir]
+        return key_remap
       end
 
-      if dir == prev_dir then
+      if key == prev_key then
         local current_time = vim.loop.hrtime()
-        local elapsed_time = (current_time - prev_time) / 1e6
+        local elapsed_time = (current_time - prev_time) / 1000000
 
         prev_time = current_time
         move_count = elapsed_time > 150 and 0 or move_count + 1
       else
         prev_time = 0
         move_count = 0
-        prev_dir = dir
+        prev_key = key
       end
-
-      local key_accel = accel[dir]
 
       for idx, count in ipairs(key_accel) do
-        if move_count < count then return idx .. chars[dir] end
+        if move_count < count then return idx .. key_remap end
       end
 
-      return #key_accel .. chars[dir]
+      return #key_accel .. key_remap
     end
 
-    vim.keymap.set({ "n", "v" }, 'h', function() return move('h') end, { expr = true })
-    vim.keymap.set({ "n", "v" }, 'j', function() return move('j') end, { expr = true })
-    vim.keymap.set({ "n", "v" }, 'k', function() return move('k') end, { expr = true })
-    vim.keymap.set({ "n", "v" }, 'l', function() return move('l') end, { expr = true })
-    vim.keymap.set({ "n", "v" }, '<Left>', function() return move('<Left>') end, { expr = true })
-    vim.keymap.set({ "n", "v" }, '<Down>', function() return move('<Down>') end, { expr = true })
-    vim.keymap.set({ "n", "v" }, '<Up>', function() return move('<Up>') end, { expr = true })
-    vim.keymap.set({ "n", "v" }, '<Right>', function() return move('<Right>') end, { expr = true })
+    km({ "n", "v" }, 'h', function() return mv('h', 'h', accel_h) end, { expr = true })
+    km({ "n", "v" }, 'j', function() return mv('j', 'gj', accel_v) end, { expr = true })
+    km({ "n", "v" }, 'k', function() return mv('k', 'gk', accel_v) end, { expr = true })
+    km({ "n", "v" }, 'l', function() return mv('l', 'l', accel_h) end, { expr = true })
+    km({ "n", "v" }, '<Left>', function() return mv('<Left>', 'g<Left>', accel_h) end, { expr = true })
+    km({ "n", "v" }, '<Down>', function() return mv('<Down>', 'g<Down>', accel_v) end, { expr = true })
+    km({ "n", "v" }, '<Up>', function() return mv('<Up>', 'g<Up>', accel_v) end, { expr = true })
+    km({ "n", "v" }, '<Right>', function() return mv('<Right>', '<Right>', accel_h) end, { expr = true })
   end,
   keys = {
     { "h",       mode = { "n", "v" } },
