46d95b4b977f827581f76b5db1a5e0da2247d477
Andis Spriņķis <andis@sprinkis.com>
2026-02-27 21:13:13 +0200

    ok

--- lua/plugins/_virt_fast_cursor_move.lua
+++ lua/plugins/_virt_fast_cursor_move.lua
@@ -2,7 +2,7 @@ local M = {
   "_virt_fast_cursor_move",
   virtual = true,
   config = function()
-    -- Accelerates movement of h, j, k, l keys
+    -- Accelerates movement of h, j, k, l and arrow keys
     -- Adapted from https://github.com/xiyaowong/fast-cursor-move.nvim (license: MIT).
 
     local fn = vim.fn
