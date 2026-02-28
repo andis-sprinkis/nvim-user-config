e5abf18f0543194676a7e35203bf359f8a060731
Andis Spriņķis <andis@sprinkis.com>
2026-02-28 11:07:05 +0200

    ok

--- lua/plugins/_virt_fast_cursor_move.lua
+++ lua/plugins/_virt_fast_cursor_move.lua
@@ -1,14 +1,15 @@
+local km = vim.keymap.set
+
 local kmd = { "n", "o", "x" }
 
 local M = {
   "_virt_fast_cursor_move",
   virtual = true,
   config = function()
-    -- Accelerates movement of h, j, k, l and arrow keys.
+    -- Accelerates movement of h, j, k, l, arrows and word-motion keys keys.
     -- Adapted from https://github.com/xiyaowong/fast-cursor-move.nvim (license: MIT).
 
     local fn = vim.fn
-    local km = vim.keymap.set
 
     local key_accel_v = { 5, 15, 45, 60, 90, 120, 160, 190, 220 }
     local key_accel_h = { 5, 30, 60, 90, 120, 160, 190, 220, 250 }
@@ -57,14 +58,17 @@ local M = {
 
     -- km(kmd, 'w', function() return mv("w", key_accel_h) end, kopt)
     km(kmd, 'w', function() return mv("<cmd>lua require('spider').motion('w')<CR>", key_accel_h) end, kopt)
+
     km(kmd, 'W', function() return mv('W', key_accel_h) end, kopt)
+
     -- km(kmd, 'e', function() return mv('e', key_accel_h) end, kopt)
     km(kmd, 'e', function() return mv("<cmd>lua require('spider').motion('e')<CR>", key_accel_h) end, kopt)
-    -- km(kmd, 'e', function() return mv('ge', key_accel_h) end, kopt)
-    km(kmd, 'e', function() return mv("<cmd>lua require('spider').motion('ge')<CR>", key_accel_h) end, kopt)
+
     km(kmd, 'E', function() return mv('E', key_accel_h) end, kopt)
+
     -- km(kmd, 'b', function() return mv('b', key_accel_h) end, kopt)
     km(kmd, 'b', function() return mv("<cmd>lua require('spider').motion('b')<CR>", key_accel_h) end, kopt)
+
     km(kmd, 'B', function() return mv('B', key_accel_h) end, kopt)
     --
   end,
@@ -89,6 +93,9 @@ local M = {
   },
   dependencies = {
     "https://github.com/chrisgrieser/nvim-spider",
+    config = function()
+      km(kmd, "ge", "<cmd>lua require('spider').motion('ge')<CR>")
+    end
   }
 }
 
