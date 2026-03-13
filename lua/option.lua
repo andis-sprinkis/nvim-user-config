5968d39357a7051a5b5ae1cdcba3be1acd5aec67
Andis Spriņķis <andis@sprinkis.com>
2026-03-13 09:48:49 +0200

    ok

--- lua/option.lua
+++ lua/option.lua
@@ -170,6 +170,16 @@ ac(
 )
 
 ac(
+  { 'BufEnter' },
+  {
+    callback = function()
+      optl.ro = false
+    end,
+    group = ag_option,
+  }
+)
+
+ac(
   'FileType',
   {
     group = ag_option,
@@ -632,8 +642,11 @@ do
   -- Accelerates movement of h, j, k, l, arrows and word-motion keys keys.
   -- Adapted from https://github.com/xiyaowong/fast-cursor-move.nvim (license: MIT).
 
-  local accel_v = { 5, 15, 45, 60, 90, 120, 160, 190, 220 }
-  local accel_h = { 5, 30, 60, 90, 120, 160, 190, 220, 250 }
+  -- Accelerates movement of h, j, k, l, arrows and word-motion keys keys.
+  -- Adapted from https://github.com/xiyaowong/fast-cursor-move.nvim (license: MIT).
+
+  local accel_v = { 5, 15, 45, 60, 90, 120, 160, 190, 220, 250, 280, 320 }
+  local accel_h = { 5, 30, 60, 90, 120, 160, 190, 220, 250, 280, 320, 350 }
 
   local prev_key
   local prev_time = 0
@@ -665,32 +678,32 @@ do
 
   local kmd = { "n", "o", "x" }
 
-  km(kmd, 'h', function() return mv('h', accel_h) end, kopt)
-  km(kmd, 'j', function() return mv('j', accel_v) end, kopt)
-  km(kmd, 'k', function() return mv('k', accel_v) end, kopt)
-  km(kmd, 'l', function() return mv('l', accel_h) end, kopt)
+    km(kmd, 'h', function() return mv('h', accel_h) end, kopt)
+    km(kmd, 'j', function() return mv('j', accel_v) end, kopt)
+    km(kmd, 'k', function() return mv('k', accel_v) end, kopt)
+    km(kmd, 'l', function() return mv('l', accel_h) end, kopt)
 
-  km(kmd, '<Left>', function() return mv('<Left>', accel_h) end, kopt)
-  km(kmd, '<Down>', function() return mv('<Down>', accel_v) end, kopt)
-  km(kmd, '<Up>', function() return mv('<Up>', accel_v) end, kopt)
-  km(kmd, '<Right>', function() return mv('<Right>', accel_h) end, kopt)
+    km(kmd, '<Left>', function() return mv('<Left>', accel_h) end, kopt)
+    km(kmd, '<Down>', function() return mv('<Down>', accel_v) end, kopt)
+    km(kmd, '<Up>', function() return mv('<Up>', accel_v) end, kopt)
+    km(kmd, '<Right>', function() return mv('<Right>', accel_h) end, kopt)
 
-  km(kmd, '-', function() return mv('-', accel_h) end, kopt)
+    km(kmd, '-', function() return mv('-', accel_h) end, kopt)
 
-  km(kmd, '=', function() return mv('+', accel_h) end, kopt)
-  km(kmd, '+', '=')
+    km(kmd, '=', function() return mv('+', accel_h) end, kopt)
+    km(kmd, '+', '=')
 
-  km(kmd, 'w', function() return mv("w", accel_h) end, kopt)
+    km(kmd, 'w', function() return mv("w", accel_h) end, kopt)
 
-  km(kmd, 'W', function() return mv('W', accel_h) end, kopt)
+    km(kmd, 'W', function() return mv('W', accel_h) end, kopt)
 
-  km(kmd, 'e', function() return mv('e', accel_h) end, kopt)
+    km(kmd, 'e', function() return mv('e', accel_h) end, kopt)
 
-  km(kmd, 'E', function() return mv('E', accel_h) end, kopt)
+    km(kmd, 'E', function() return mv('E', accel_h) end, kopt)
 
-  km(kmd, 'b', function() return mv('b', accel_h) end, kopt)
+    km(kmd, 'b', function() return mv('b', accel_h) end, kopt)
 
-  km(kmd, 'B', function() return mv('B', accel_h) end, kopt)
+    km(kmd, 'B', function() return mv('B', accel_h) end, kopt)
   --
 end
 
