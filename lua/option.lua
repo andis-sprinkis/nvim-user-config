603e3f77c6bfc71c8312f7f0ffb23766a0c46e94
Andis Spriņķis <andis@sprinkis.com>
2026-03-27 13:05:31 +0200

    ok

--- lua/option.lua
+++ lua/option.lua
@@ -589,7 +589,7 @@ do
 
     local function open_variant(variant, is_fp, with_vim_ui_open)
       if is_fp then
-        variant = fn.resolve(fn.expand(variant))
+        variant = fn.resolve(variant)
 
         local success, err, err_name = uv.fs_stat(variant)
 
