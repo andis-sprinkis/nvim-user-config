922c7947a195b38635eaad432cdb5c120ce3afa9
Andis Spriņķis <andis@sprinkis.com>
2026-04-05 17:27:11 +0300

    ok

--- lua/plugins/_virt_inactive_stop_lsp.lua
+++ lua/plugins/_virt_inactive_stop_lsp.lua
@@ -15,37 +15,6 @@ local M = {
     local lsp_start_retries_timeout = 1000  -- ms
     local lsp_stop_grace_period = (60 * 15) -- seconds
 
-    -- Start LSP clients for the current buffer.
-    local function start_lsp()
-      local lsp_start_timer = vim.uv.new_timer()
-      local elapsed_retries = 0
-
-      local start_lsp_timer_callback
-      start_lsp_timer_callback = vim.schedule_wrap(function()
-        if elapsed_retries >= lsp_start_retries then
-          lsp_start_timer:stop()
-          lsp_start_timer:close()
-          return
-        end
-
-        vim.cmd(":LspStart")
-        elapsed_retries = elapsed_retries + 1
-        lsp_start_timer:start(lsp_start_retries_timeout, 0, start_lsp_timer_callback)
-      end)
-
-      lsp_start_timer:start(lsp_start_retries_timeout, 0, start_lsp_timer_callback)
-    end
-
-    -- Stop all LSP clients for all buffers.
-    local function stop_lsp()
-      for _, client in pairs(vim.lsp.get_clients()) do
-        if not vim.tbl_contains(excluded_lsp_clients, client.name) then
-          vim.lsp.stop_client(client.id)
-          client.rpc.terminate()
-        end
-      end
-    end
-
     local focus_lost_timer = vim.uv.new_timer()
     local lsp_start_time = os.time()
     local focus_lost_current_time = 0
@@ -68,7 +37,15 @@ local M = {
 
               if lsp_stop_grace_period_exceeded and not lsp_has_been_stopped then
                 focus_lost_timer:stop()
-                stop_lsp()
+
+                -- Stop all LSP clients for all buffers.
+                for _, client in pairs(vim.lsp.get_clients()) do
+                  if not vim.tbl_contains(excluded_lsp_clients, client.name) then
+                    client:stop()
+                    client.rpc.terminate()
+                  end
+                end
+
                 lsp_has_been_stopped = true
               end
             end
@@ -84,7 +61,32 @@ local M = {
         vim.defer_fn(
           function()
             if not lsp_start_focus_delay_counting then return end
-            if lsp_has_been_stopped then start_lsp() end
+
+            if lsp_has_been_stopped then
+              -- Start LSP clients for the current buffer.
+
+              local lsp_start_timer = vim.uv.new_timer()
+              local elapsed_retries = 0
+
+              local start_lsp_timer_callback
+
+              start_lsp_timer_callback = vim.schedule_wrap(function()
+                if elapsed_retries >= lsp_start_retries then
+                  lsp_start_timer:stop()
+                  lsp_start_timer:close()
+                  return
+                end
+
+                for _, client in pairs(vim.lsp.get_clients({ bufnr = 0 })) do
+                  vim.lsp.enable(client.name)
+                end
+
+                elapsed_retries = elapsed_retries + 1
+                lsp_start_timer:start(lsp_start_retries_timeout, 0, start_lsp_timer_callback)
+              end)
+
+              lsp_start_timer:start(lsp_start_retries_timeout, 0, start_lsp_timer_callback)
+            end
 
             lsp_start_time = os.time()
             focus_lost_current_time = 0
