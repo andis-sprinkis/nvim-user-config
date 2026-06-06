f76fb1f1eadb4be43d3a1a8d2707425291b2ece7
Andis Spriņķis <andis@sprinkis.com>
2026-06-07 01:17:44 +0300

    ok

--- lua/plugins/mini_comment.lua
+++ lua/plugins/mini_comment.lua
@@ -1,7 +1,7 @@
 local M = {
   'https://github.com/nvim-mini/mini.comment',
   branch = 'main',
-  commit = 'a0c721115faff8d05505c0a12dab410084d9e536',
+  commit = 'fc87ba6554f182161d9a4bab5017c575571f000f',
   event = 'VeryLazy'
 }
 
