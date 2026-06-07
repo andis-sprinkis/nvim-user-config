ad712550b94e1e30297a9a5ae178ddade72f40cb
Andis Spriņķis <andis@sprinkis.com>
2026-06-07 18:56:23 +0300

    ok

--- lua/plugins/hlargs_nvim.lua
+++ lua/plugins/hlargs_nvim.lua
@@ -1,7 +1,7 @@
 local M = {
   'https://github.com/m-demare/hlargs.nvim',
   branch = 'main',
-  commit = '0b29317c944fb1f76503ce4540d6dceffbb5ccd2',
+  commit = '05f3d1789642d5e1807121c05c42a5e883ba46d3',
   config = function()
     require('hlargs').setup {
       hl_priority = 200,
