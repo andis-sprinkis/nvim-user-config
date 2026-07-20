05ce29a30a5cda05aebd389518f75d7d3345247f
Andis Spriņķis <andis@sprinkis.com>
2026-07-20 22:22:22 +0300

    ok

--- lua/plugins/vim_illuminate.lua
+++ lua/plugins/vim_illuminate.lua
@@ -1,7 +1,7 @@
 local M = {
   'https://github.com/RRethy/vim-illuminate',
   branch = 'master',
-  commit = '0d1e93684da00ab7c057410fecfc24f434698898',
+  commit = '91313e598ca62e110bc71535c49069b66b9883c9',
   config = function()
     require('illuminate').configure({
       filetypes_denylist = {},
