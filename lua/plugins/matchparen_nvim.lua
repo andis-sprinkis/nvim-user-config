7ec7aa70cf618a15d0c6805442025e24cadb93d3
Andis Spriņķis <andis@sprinkis.com>
2026-07-20 22:19:24 +0300

    ok

--- lua/plugins/matchparen_nvim.lua
+++ lua/plugins/matchparen_nvim.lua
@@ -1,23 +1,23 @@
 local M = {
   'https://github.com/monkoose/matchparen.nvim',
   branch = 'main',
-  commit = 'd48619803d5e2aacc207b46ce6acfb23bcdb9b1d',
+  commit = '75398158ea5a4cee4f0cbbeefdbf8fb1c3343e28',
   config = function()
     require("matchparen").setup()
 
-    local ag = vim.api.nvim_create_augroup
-    local ac = vim.api.nvim_create_autocmd
-
-    ac({ 'BufRead' }, {
-      callback = function()
-        if vim.b.largef then
-          vim.cmd("MatchParenDisable")
-        else
-          vim.cmd("MatchParenEnable")
-        end
-      end,
-      desc = "TODO DESC (user)"
-    })
+    vim.api.nvim_create_autocmd(
+      'BufRead',
+      {
+        callback = function()
+          if vim.b.largef then
+            vim.cmd("MatchParenDisable")
+          else
+            vim.cmd("MatchParenEnable")
+          end
+        end,
+        desc = "TODO DESC (user)"
+      }
+    )
   end,
   event = 'VeryLazy'
 }
