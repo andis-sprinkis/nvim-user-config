a9f79857145d42f3358f5222490e95c67c2116d7
Andis Spriņķis <andis@sprinkis.com>
2026-08-07 09:28:49 +0300

    ok

---
+++ lua/plugins/celeste_comment_nvim.lua
@@ -0,0 +1,10 @@
+local M = {
+  "https://github.com/celeste3z/celeste_comment.nvim",
+  branch = 'main',
+  commit = '577b2cf012ed2f2f5739d1162d7c2e4a710e860a',
+  lazy = false,
+  opts = {},
+  dependencies = { 'https://github.com/nvim-treesitter/nvim-treesitter' }
+}
+
+return M
--- lua/plugins/mini_comment.lua
+++
@@ -1,8 +0,0 @@
-local M = {
-  'https://github.com/nvim-mini/mini.comment',
-  branch = 'main',
-  commit = 'fc87ba6554f182161d9a4bab5017c575571f000f',
-  event = 'VeryLazy'
-}
-
-return M
