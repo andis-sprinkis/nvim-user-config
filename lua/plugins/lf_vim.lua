869f0367027f2c1740e8a5f2175e802363d8656f
Andis Spriņķis <andis@sprinkis.com>
2026-08-07 08:27:26 +0300

    ok

--- lua/plugins/lf_vim.lua
+++ lua/plugins/lf_vim.lua
@@ -1,7 +1,7 @@
 local M = {
   'https://github.com/andis-sprinkis/lf-vim',
   branch = 'master',
-  commit = '549286d0677c0c6c252b59644b3868c94ee64a7a',
+  commit = 'e7282e9a0c6fa776f078c6355cb7badc4d76f04d',
   event = { 'BufReadPre lfrc' }
 }
 
